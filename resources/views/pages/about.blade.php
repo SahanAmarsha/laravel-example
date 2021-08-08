3f42b4f09236826d019426c1bc8da7701bdadc9a
Sahan Amarsha <sahanamarsha@gmail.com>
2021-08-08 14:30:25 +0530

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{config('app.name', 'LSAPP')}}</title>
+</head>
+<body class="antialiased">
+    <h1>About</h1>
+    <h4>This is the about page</h4>
+</body>
+</html>
